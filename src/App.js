40d3aa855e9cc32eb83d28b1a1dedd19e7555410
raymondeyan <raymondyan777@gmail.com>
2021-10-15 23:41:29 -0400

    added some web audio elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
- 
+import React from 'react';
+import Osc1 from './Osc1';
 import './App.scss';
 
+let actx = new AudioContext();
+let out = actx.destination;
+let osc1 = actx.createOscillator();
+let gain1 = actx.createGain();
+osc1.connect(gain1);
+gain1.connect(out);
+
 function App() {
-  return (
-    <div className="App">
-      <h1>slider</h1>
-    </div>
-  );
+  return <div className="App">
+  <h1>slider</h1>
+  <button  onClick={() => osc1.start()}>start</button>
+  <button onClick={() => osc1.stop()}>Stop</button>
+  <Osc1 />
+</div>;
 }
 
 export default App;
---
+++ src/Osc1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Osc1 = () => {
+  return (
+    <div>
+      <input type="range" id="frequency"/>
+    </div>
+  )
+}
+
+export default Osc1
